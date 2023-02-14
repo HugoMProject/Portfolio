8c8d0494732bfba2cceff274531883240ef83696
HugoMProject <hugomoyano1996@gmail.com>
2023-02-13 21:43:29 -0300

    firt commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './section/header/header.component';
+import { BannerComponent } from './section/banner/banner.component';
+import { SkillsComponent } from './section/skills/skills.component';
+import { ProjectComponent } from './section/project/project.component';
+import { AboutMeComponent } from './section/about-me/about-me.component';
+import { FooterComponent } from './section/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    SkillsComponent,
+    ProjectComponent,
+    AboutMeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
