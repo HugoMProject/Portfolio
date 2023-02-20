3ec98a19322e692b17c427aca2613c326bd0c048
HugoMProject <hugomoyano1996@gmail.com>
2023-02-19 21:22:34 -0300

    se agrego animaciones ademas de las imagenes

--- src/app/section/banner/banner.component.ts
+++ src/app/section/banner/banner.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,state,animate,transition,style } from '@angular/animations'
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations:[
+    trigger('animateName',[
+      state('void',style({
+        opacity:0
+      })),
+      transition(':enter',[
+        animate('1.5s ease-in',style({
+          opacity:1,
+        })
+        )
+      ])
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
 
