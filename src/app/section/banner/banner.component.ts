76459108b70f0c6506391dde364c330c5844bb3e
HugoMProject <hugomoyano1996@gmail.com>
2023-02-23 15:08:21 -0300

    se dio estilos a la seccion de about me y de footer

--- src/app/section/banner/banner.component.ts
+++ src/app/section/banner/banner.component.ts
@@ -24,6 +24,18 @@ export class BannerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.moveImg()
   }
-
+  
+  moveImg(){
+    //funcion para mover el div coon imagen, para darle un efecto de animacion
+    window.onload = ()=>{
+      let imgDiv:any = document.getElementById('id-img');
+      let idH1:any = document.getElementById('id-h1');
+      idH1.style.top = "240px";
+      idH1.style.transitionDuration = "1.5s";
+      imgDiv.style.left = "60%";
+      imgDiv.style.transitionDuration = "1.5s";
+    }
+  }
 }
